7fed2ed9ec317b84fe2ef6e9fa811f956e651ee1
sarah441 <sarahibrahim3477@gmail.com>
2024-08-13 15:34:54 +0300

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-To know the count of subscribers
-"""
-
+"""Function to count subscribers of a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """to handle errors"""
-    if subreddit is None or type(subreddit) is not str:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/IllBuilder2529)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    about = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-                             v1.0.0 (by /u/IllBuilder2529)'}).json()
-    subs_count = about.get("data", {}).get("subscribers", 0)
-    return subs_count
+    count = response.json().get("data")
+    return count.get("subscribers")
