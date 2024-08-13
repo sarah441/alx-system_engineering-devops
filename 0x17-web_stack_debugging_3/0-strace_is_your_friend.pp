2265375557f6aac295ef3b502b96326d902acebe
sarah441 <sarahibrahim3477@gmail.com>
2024-08-13 16:29:25 +0300

    f

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# puppet fix to find why Apache is returning a 500 error
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  path    => ['/bin','/usr/bin']
+}
