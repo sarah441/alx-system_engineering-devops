75f254078838d0f51c9d13d934f40869c12f3e9e
sarah441 <sarahibrahim3477@gmail.com>
2024-08-21 18:54:13 +0300

    dd

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Solve that it can open alot of files at the same time
+
+exec {'restart1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+} ->
+
+exec {'restart2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
